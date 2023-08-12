c405ec3694fed1d73edd8c6ca25615874ee719af
dev.ankur <ankurbhati9027@gmail.com>
2023-08-12 23:22:58 +0530

    making some changes in home controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,16 @@
 const Student = require("../models/student");
 
 module.exports.home = async (req, res) => {
-  const data = await Student.find({}).catch((err) => {
+  const data = await Student.find({})
+    .then((data) => {
+       res.render("students_list", {
+        students: data,
+      });
+      
+    })
+    
+    .catch((err) => {
     console.log("Err in students find", err);
   });
-
-  return res.render("students_list", {
-    students: data,
-  });
+return;
 };
