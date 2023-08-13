c4f1852669411cb93b33ef946922dcbaabccc639
dev.ankur <ankurbhati9027@gmail.com>
2023-08-13 07:31:18 +0530

    all err fixed

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 async function main() {
   try {
-    await mongoose.connect(
-      process.env.MONGOOSE_URI
-    );
+    await mongoose.connect(process.env.MONGOOSE_URI || "mongodb://127.0.0.1/csv_project_db");
 
     console.log("Connected to mongoDB successfully....");
   } catch (error) {
